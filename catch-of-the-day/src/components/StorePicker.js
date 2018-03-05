d0097b265beac3b44a96398f582e20029d20e375
Carlos Martinez <martinezcj2@gmail.com>
2018-03-04 22:11:48 -0800

    Add input and button to form

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,8 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class StorePicker extends Component {
   render() {
-    return <p>Hello</p>
+    return (
+      // Fragment allows adjacent elements, returns no element
+      <Fragment>
+        <form className='store-selector' action="">
+          <h2>Hey</h2>
+          <input type="text" required placeholder='Store Name' />
+          <button type='submit'>Visit Store</button>
+        </form>
+      </Fragment>
+    )
   }
 }
 
