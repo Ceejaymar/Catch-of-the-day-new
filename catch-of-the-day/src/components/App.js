e52066239e972cb71657d671879e2f1ee0e23f05
Carlos Martinez <martinezcj2@gmail.com>
2018-03-04 22:12:49 -0800

    Create Add component

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+        <Header />
+        </div>
+        <Inventory />
+        <Order />
+      </div>
+    )
+  }
+}
+
+export default App;
