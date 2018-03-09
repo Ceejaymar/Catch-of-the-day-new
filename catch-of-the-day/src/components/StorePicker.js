b2384b06852c8e2edd1bc33b6ab996dce459c0ed
Carlos Martinez <martinezcj2@gmail.com>
2018-03-08 20:50:14 -0800

    Add onSubmit to listen to form

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,13 +1,27 @@
 import React, { Component, Fragment } from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
+  myInput = React.CreateRef();
+  constructor() {
+    super()
+
+    // this.goToStore = this.goToStore.bind(this);
+  }
+
+  goToStore = (e) => { //can be written this way to bind this property to this, instead of line #9
+    e.preventDefault()
+
+    console.log("At the store");
+  }
+
   render() {
     return (
       // Fragment allows adjacent elements, returns no element
       <Fragment>
-        <form className='store-selector' action="">
+        <form onSubmit={this.goToStore} className='store-selector' action="">
           <h2>Hey</h2>
-          <input type="text" required placeholder='Store Name' />
+          <input ref={this.myInput} type="text" required placeholder='Store Name' defaultValue={getFunName()} />
           <button type='submit'>Visit Store</button>
         </form>
       </Fragment>
