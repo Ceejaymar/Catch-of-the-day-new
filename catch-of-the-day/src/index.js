28811e535e875141bcc7727e607f9b03229d2050
Carlos Martinez <martinezcj2@gmail.com>
2018-03-08 20:50:59 -0800

    Import Router and remove unnecessary imports

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Router from './components/Router';
 
-import StorePicker from './components/StorePicker';
-import App from './components/App';
 import './css/style.css';
 
 render(
-  <App />,
+  <Router />,
   document.querySelector('#main')
 )
