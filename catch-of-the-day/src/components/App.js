bafb6eadcf1570debb3164c16dfe6c6943777449
Carlos Martinez <martinezcj2@gmail.com>
2018-03-11 12:44:16 -0700

    Create loadfish property

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,8 +2,27 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends Component {
+  // Alternative way of creating state
+  state = {
+    fishes: {},
+    order: {}
+  }
+
+  addFish = fish => {
+    const fishes = {...this.state.fishes}
+
+    fishes[`fish${Date.now()}`] = fish;
+
+    this.setState({ fishes })
+  }
+
+  loadSampleFishes = () => { // Any methods that update state should live in the main 'app' where state is created
+    this.setState({ fishes: sampleFishes })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +30,7 @@ class App extends Component {
           <Header tagline='Seafood' />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish ={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     )
   }
