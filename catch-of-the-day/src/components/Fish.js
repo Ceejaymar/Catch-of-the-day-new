581dbea909e4e9e214c254a69a8652f34085f4a5
Carlos Martinez <martinezcj2@gmail.com>
2018-03-11 23:47:10 -0700

    Add onClick to button, Add ternary to variable

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 import { formatPrice } from '../helpers';
 
 class Fish extends Component {
+  handleClick = () => {
+    this.props.addToOrder(this.props.index);
+  }
+  
   render() {
     const {name, price, image, status, desc} = this.props.details;
+    const isAvailable = status === 'available';
+    const buttonText = isAvailable ? 'Add to Cart' : 'Sold Out!' ;
 
     return (
       <li className="menu-fish">
@@ -13,7 +19,7 @@ class Fish extends Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add to Cart</button>
+        <button disabled={!isAvailable} onClick={this.handleClick}>{ buttonText }</button>
       </li>
     )
   }
